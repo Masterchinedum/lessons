0abe27632eb27bfd9115e138141c9b5de353a1e5
Masterchinedum <chinedu02k@gmail.com>
2024-02-08 00:57:45 +0100

    Commented out unused code in 2-modules.js

--- 2-modules.js
+++ 2-modules.js
@@ -4,9 +4,9 @@
 // const john = 'john';
 // const peter = 'peter'
 
-const sayHi = (name) => {
-    console.log(`Hello there ${name}`);
-}
+// const sayHi = (name) => {
+//     console.log(`Hello there ${name}`);
+// }
 
 sayHi('susan')
 sayHi(john)
