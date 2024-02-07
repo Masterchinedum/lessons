49e026e2b66567d085d9c811bf70b5396babe19b
Masterchinedum <chinedu02k@gmail.com>
2024-02-08 00:49:53 +0100

    Add interval code to execute code at intervals

--- 1-intro.js
+++ 1-intro.js
@@ -6,4 +6,10 @@ if (number < 10) {
     console.log("greater number");
 }
 
+setInterval(() => {
+    // Code to be executed at intervals
+    console.log("Running code at intervals");
+}, 1000); // Interval in milliseconds (e.g., 1000ms = 1 second)
+
+
 console.log("I am learning Node.js")
