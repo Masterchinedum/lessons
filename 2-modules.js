5a8f3e7179d1c38d1d0cc7cffceaa6ab79c87521
Masterchinedum <chinedu02k@gmail.com>
2024-02-08 00:56:31 +0100

    Remove commented out code in 2-modules.js

--- 2-modules.js
+++ 2-modules.js
@@ -1,8 +1,8 @@
 //Modules - Encapsulates code(only share minimum)
 //CommonJS, every file is module (by default)
-const secret = 'SUPER SECRETE'
-const john = 'john';
-const peter = 'peter'
+// const secret = 'SUPER SECRETE'
+// const john = 'john';
+// const peter = 'peter'
 
 const sayHi = (name) => {
     console.log(`Hello there ${name}`);
---
+++ 3-names.js
@@ -0,0 +1,3 @@
+const secret = 'SUPER SECRETE'
+const john = 'john';
+const peter = 'peter'
