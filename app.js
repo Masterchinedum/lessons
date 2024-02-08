431cf65d8c15289b33b91f13ba23002702616aae
Masterchinedum <chinedu02k@gmail.com>
2024-02-08 14:59:15 +0100

    Merge the two files into one and save it to disk

--- app.js
+++ app.js
@@ -1,25 +1,7 @@
-const os = require('os')
+const {readFileSync, writeFileSync} = require('fs');
 
-//info about user
-const user = os.userInfo()
-// console.log(user)
+const first = readFileSync('./content/first.txt', 'utf8');
+const second = readFileSync('./content/second.txt', 'utf8');
 
-//method returns the system uptime in seconds
-// console.log(`The System Uptime is ${(os.uptime()) } seconds`)
-
-// console.log(`The System Uptime is ${(os.uptime() / (60 * 60 )) } hours`)
-
-// console.log(`The System Uptime is ${(os.uptime() / (60 * 60 * 24)) } days`)
-
-
-const currentOS = {
-    type: os.type(), //returns the operating system name
-    platform: os.platform(), //returns the running OS name
-    arch: os.arch(), //returns CPU architecture
-    release: os.release(), //returns the kernel version string
-    hostname: os.hostname(), //returns the network name of the machine
-    totalMem: os.totalmem(), //returns the system memory in bytes
-    freeMem: os.freemem(), //returns the free system memory in bytes
-    uptime: os.uptime(), //returns the system uptime in seconds
-}
-console.dir(currentOS);
+// Merge the two files into one and save it to disk
+writeFileSync('./content/result-sync.txt', `Here is the result: ${first}, ${second}`, {flag: 'a'});
