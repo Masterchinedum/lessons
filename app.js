fef38239e20c27e489cc37a87bd3f015737bbaf9
Masterchinedum <chinedu02k@gmail.com>
2024-02-08 16:55:12 +0100

    Refactor file reading and logging in app.js

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-const {readFileSync, writeFileSync} = require('fs');
+const {readFile, writeFile} = require('fs');
 
-const first = readFileSync('./content/first.txt', 'utf8');
-const second = readFileSync('./content/second.txt', 'utf8');
-
-// Merge the two files into one and save it to disk
-writeFileSync('./content/result-sync.txt', `Here is the result: ${first}, ${second}`, {flag: 'a'});
+readFile('./content/first.txt', 'utf8', (err, result) => {
+    if(err){
+        console.log(err)
+        return
+    }
+    console.log(result)
+})
